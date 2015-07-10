293d63fa2b85e3732a36ebc2ff2c92dd7093f149
Mike Land <mikepland@gmail.com>
2015-07-10 03:15:45 -0500

    sets up label for phone app

--- HNonAppleWatch/ViewController.swift
+++ HNonAppleWatch/ViewController.swift
@@ -9,9 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let homeScreenLabel: UILabel = UILabel()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.homeScreenLabel.frame = CGRectMake(0, 30, self.view.frame.width, 30)
+        self.homeScreenLabel.text = "Hacker New on Apple Watch"
+        self.homeScreenLabel.textAlignment = NSTextAlignment.Center
+        self.view.addSubview(homeScreenLabel)
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
